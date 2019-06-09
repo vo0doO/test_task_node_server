b22530752812950ea303a073c45e47245e82b726
vo0doo <kirsanov.bvt@gmail.com>
2019-06-09 16:01:00 +0300

    Интерфейс Биллс класс БиллсМодель и функция getItems

---
+++ app/models/bills.ts
@@ -0,0 +1,17 @@
+import {PgService} from "innots";
+import {pgService} from "../../app";
+
+export interface IBills {
+    idBills: bigint;
+    billsCount: bigint;
+    billsAmount: number;
+    billsPaidCount: bigint;
+    billsPaindAmount: bigint;
+    billsAddTimestamp: string;
+}
+
+export class BillsModel {
+    public async getItems(): Promise<Array<IBills>> {
+        return await pgService.getRows(`SELECT * FROM public.aggr_bills`);
+    }
+}
