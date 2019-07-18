cf902700e6dcf297ef741872021f258609670412
vo0doo <kirsanov.bvt@gmail.com>
2019-07-18 08:23:28 +0000

    тест функции создания рандомныйх дат

---
+++ lib/test/random_date.spec.ts
@@ -0,0 +1,18 @@
+import { AssertStatic } from '@types/chai';
+import { assert } from "chai";
+import { makeRandomDate, t } from './random_date';
+import { requestConfig } from "./request_config";
+
+export function makeRandomDateTest(repeat:number): AssertStatic {
+    let i =0;
+    while(i<repeat){
+        setTimeout(function test() {
+            let [rmin, rmax] = makeRandomDate(requestConfig.maxDate, requestConfig.minDate)
+            assert.isAbove(rmax.getTime(), rmin.getTime())
+            assert.isAtLeast(t(rmin), t(requestConfig.minDate))
+            assert.isAtMost(t(rmax),t(requestConfig.maxDate))
+        }, 50)
+    i++
+}}
+
+makeRandomDateTest(1000)
